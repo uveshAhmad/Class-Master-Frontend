41c0821ce337e11355a12258b90df3f97c6c3e30
Uvesh Ahmad <uveshahmad30@gmail.com>
2025-01-18 23:28:09 +0530

    upfate path

--- src/store/axios.js
+++ src/store/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-    baseURL: "http://localhost:8008/api"
+    baseURL: "https://class-master-back-end.vercel.app/api"
 })
 
 export default instance
